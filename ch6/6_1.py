baf7fc71447a8d10513c506f5d42cbfc02d7e320
seram2002-wq <seram2002@ewhain.net>
2026-09-22 10:29:35 +0900

    몬테카를로 시뮬레이션

---
+++ ch6/6_1.py
@@ -0,0 +1,44 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from Investar import Analyzer
+
+mk = Analyzer.MarketDB()
+stocks = ['삼성전자', 'SK하이닉스', '현대자동차', 'NAVER']
+df = pd.DataFrame()
+for s in stocks:
+    df[s] = mk.get_daily_price(s, '2022-08-19', '2026-09-22')['close']
+  
+daily_ret = df.pct_change() 
+annual_ret = daily_ret.mean() * 252
+daily_cov = daily_ret.cov() 
+annual_cov = daily_cov * 252
+
+port_ret = [] 
+port_risk = [] 
+port_weights = [] 
+
+for _ in range(20000): 
+    weights = np.random.random(len(stocks)) 
+    weights /= np.sum(weights) 
+
+    returns = np.dot(weights, annual_ret) 
+    risk = np.sqrt(np.dot(weights.T, np.dot(annual_cov, weights))) 
+
+    port_ret.append(returns) 
+    port_risk.append(risk) 
+    port_weights.append(weights) 
+
+portfolio = {'Returns': port_ret, 'Risk': port_risk} 
+for i, s in enumerate(stocks): 
+    portfolio[s] = [weight[i] for weight in port_weights] 
+df = pd.DataFrame(portfolio) 
+df = df[['Returns', 'Risk'] + [s for s in stocks]] 
+
+df.plot.scatter(x='Risk', y='Returns', figsize=(8, 6), grid=True)
+plt.title('Efficient Frontier') 
+plt.xlabel('Risk') 
+plt.ylabel('Expected Returns') 
+plt.show() 
